27b750e3aa9c1726c26145f523904d345730e966
NandhiniKanagasabesan <91875569+NandhiniKanagasabesan@users.noreply.github.com>
2023-10-18 12:34:04 +0200

    Add files via upload

---
+++ Preprocessing_FCSfiles.R
@@ -0,0 +1,235 @@
+####################################################
+
+# Date : 19/09/2023 #
+# Preprocessing the FCS files before importing them into cytotree ##
+# Some samples have laser names and some antibody names #
+# data transformation: Arcsinh transformation with co-factor 150 #
+# median centering and scaling # 
+
+####################################################
+
+# Set a working directory
+setwd("~/Suzanne/Cytotree/NSCLC_samples/")
+
+# Loading packages
+library(CytoTree)
+library(flowCore)
+library(stringr)
+library(scales)
+
+
+## Antibody labels ##
+# Create an empty list to store the processed flowframes
+fcs_from_file = list()
+# Donors list
+donor_list = c("028(IV)","029(IV)") ## or do i enter all the sasmple number with antibody labels
+# Get the directory of FCS 
+fcs.files = list.files("~/Suzanne/Cytotree/NSCLC_late_stage_samples/Data/late_stage_nsclc/Ab_labels_raw_late_stage_samples", pattern=".fcs",full.names = TRUE)
+
+# loop to alter the expression matrices columns with the selcted list of markers
+for (i in 1:length(fcs.files)) {
+  
+  # input the FCS files 
+  fcs_from_file[i] = flowCore::read.FCS(fcs.files[i],transformation = F) 
+  
+  # List of new labels to be replaced
+  recol = c(`FSC-A` = "FSC-A",
+            `SSC-A` = "SSC-A",
+            `FJComp-APC-A` = "CD68",
+            `FJComp-APC-Cy7-A` = "Live/Dead",
+            `FJComp-Alexa Fluor 700-A` = "CD33",
+            `FJComp-Brilliant Violet 421-A` = "CD11b",
+            `FJComp-Brilliant Violet 510-A` = "CD3",
+            `FJComp-Brilliant Violet 605-A` = "CD15",
+            `FJComp-FITC-A` = "HLA-DR",
+            `FJComp-PE-A` = "PDL-1",
+            `FJComp-PE-Cy7-A` = "CD14",
+            `FJComp-PerCP-Cy5-5-A` = "CD11c",
+            `FJComp-Qdot 585-A` = "CD1a",
+            `FJComp-Qdot 625-A` = "CD16",
+            `FJComp-Qdot 705-A` = "CD19/CD20",
+            `FJComp-Qdot 800-A` = "CD45")
+  # Rename the columns with the desired marker labels
+  colnames(fcs_from_file[[i]]@exprs)[match(names(recol), colnames(fcs_from_file[[i]]@exprs))] = recol
+  fcs_from_file[[i]]@exprs = fcs_from_file[[i]]@exprs[, recol]
+  
+  # Removing the live/dead and CD45 columns; since the samples were pre-gated for live/dead and CD45
+  fcs_from_file[[i]]@exprs = fcs_from_file[[i]]@exprs[,!colnames(fcs_from_file[[i]]@exprs) %in% c("CD45", "Live/Dead")]
+  
+  # Arcsinh transformtion with co-factor 150
+  fcs_from_file[[i]]@exprs = asinh(fcs_from_file[[i]]@exprs/150) 
+  
+  # After arsinh transformation, median centering and scaling
+  center_scale = function(x) {
+    scale(x, center = apply(x,2,median), scale = T)
+  }
+  fcs_from_file[[i]]@exprs = center_scale(fcs_from_file[[i]]@exprs)
+  
+  # Also selecting the only the necessary markers in the parameters list as well
+  re_select = c("FSC-A",
+                "SSC-A",
+                "FJComp-APC-A",
+                "FJComp-Alexa Fluor 700-A",
+                "FJComp-Brilliant Violet 421-A",
+                "FJComp-Brilliant Violet 510-A",
+                "FJComp-Brilliant Violet 605-A",
+                "FJComp-FITC-A",
+                "FJComp-PE-A",
+                "FJComp-PE-Cy7-A",
+                "FJComp-PerCP-Cy5-5-A",
+                "FJComp-Qdot 585-A",
+                "FJComp-Qdot 625-A",
+                "FJComp-Qdot 705-A")
+  fcs_from_file[[i]]@parameters@data = fcs_from_file[[i]]@parameters@data[fcs_from_file[[i]]@parameters@data$name %in% re_select,]
+  
+  # Save the new pre-processed FCS files 
+  write.FCS(fcs_from_file[[i]],
+            filename = paste0("~/Suzanne/Cytotree/NSCLC_late_stage_samples/Data/late_stage_nsclc/preprocessed_late_stage_samples_v1/CLEAN_late_Mix 4_Tumor_NSCLC_with_comp_median_",donor_list[i],'.fcs'))
+}
+
+### Laser name labels ###
+# Create an empty list to store the processed flowframes
+fcs_from_file = list()
+# Donors list
+donor_list = c("03","04","06","08","09","010","011","013","014","016","017","019","023","025","026","030","045(IV).2")
+# Get the directory of FCS 
+fcs.files = list.files("~/Suzanne/Cytotree/NSCLC_late_stage_samples/Data/late_stage_nsclc/laser_labels_raw_late_stage_samples/", pattern=".fcs",full.names = TRUE)
+
+
+#loop to alter the expression matrices columns with thhe selcted list of markers
+for (i in 1:length(fcs.files)) {
+  
+  # input the FCS files 
+  fcs_from_file[i] = flowCore::read.FCS(fcs.files[i],transformation = F) 
+  
+  # Re-arrange the columns of the expression matrix
+  re_order = c("FSC-A",
+               "FSC-H",
+               "FSC-W",
+               "SSC-A",
+               "SSC-H",
+               "SSC-W",
+               "FJComp-637 pmt C 670-30-A",
+               "FJComp-637 pmt A 780-60-A",
+               "FJComp-637 pmt B 710-40-A",
+               "FJComp-405 pmt H 431-28-A",
+               "FJComp-405 pmt G 470-20-A",
+               "FJComp-405 pmt E 610-20-A",
+               "FJComp-488 pmt E 524-24-A",
+               "FJComp-561 pmt E 586-15-A",
+               "FJComp-561 pmt A 780-60-A",
+               "FJComp-488 pmt B 710-50-A",
+               "FJComp-355 pmt G 379-28-A",
+               "FJComp-355 pmt F 515-30-A",
+               "FJComp-355 pmt B 740-35-A",
+               "FJComp-355 pmt A 810-44-A",
+               "Time")
+  
+  # Re-order the column by "re_order" list
+  fcs_from_file[[i]] =  fcs_from_file[[i]][,re_order]
+  
+  # Rename the columns to match the samples with antibody labels
+  colnames(fcs_from_file[[i]]@exprs) = c("FSC-A",
+                                         "FSC-H",
+                                         "FSC-W",
+                                         "SSC-A",
+                                         "SSC-H",
+                                         "SSC-W",
+                                         "FJComp-APC-A",
+                                         "FJComp-APC-Cy7-A",
+                                         "FJComp-Alexa Fluor 700-A",
+                                         "FJComp-Brilliant Violet 421-A",
+                                         "FJComp-Brilliant Violet 510-A",
+                                         "FJComp-Brilliant Violet 605-A",
+                                         "FJComp-FITC-A",
+                                         "FJComp-PE-A",
+                                         "FJComp-PE-Cy7-A",
+                                         "FJComp-PerCP-Cy5-5-A",
+                                         "FJComp-Qdot 585-A",
+                                         "FJComp-Qdot 625-A",
+                                         "FJComp-Qdot 705-A",
+                                         "FJComp-Qdot 800-A" ,
+                                         "Time" )
+  
+  # List of new labels to be replaced
+  recol = c(`FSC-A` = "FSC-A",
+            `SSC-A` = "SSC-A",
+            `FJComp-APC-A` = "CD68",
+            `FJComp-APC-Cy7-A` = "Live/Dead",
+            `FJComp-Alexa Fluor 700-A` = "CD33",
+            `FJComp-Brilliant Violet 421-A` = "CD11b",
+            `FJComp-Brilliant Violet 510-A` = "CD3",
+            `FJComp-Brilliant Violet 605-A` = "CD15",
+            `FJComp-FITC-A` = "HLA-DR",
+            `FJComp-PE-A` = "PDL-1",
+            `FJComp-PE-Cy7-A` = "CD14",
+            `FJComp-PerCP-Cy5-5-A` = "CD11c",
+            `FJComp-Qdot 585-A` = "CD1a",
+            `FJComp-Qdot 625-A` = "CD16",
+            `FJComp-Qdot 705-A` = "CD19/CD20",
+            `FJComp-Qdot 800-A` = "CD45")
+  colnames(fcs_from_file[[i]]@exprs)[match(names(recol), colnames(fcs_from_file[[i]]@exprs))] = recol
+  fcs_from_file[[i]]@exprs = fcs_from_file[[i]]@exprs[, recol]
+  
+  # Remove the live/dead and CD45 columns
+  fcs_from_file[[i]]@exprs = fcs_from_file[[i]]@exprs[,!colnames(fcs_from_file[[i]]@exprs) %in% c("CD45", "Live/Dead")]
+
+  #  Arcsinh transformtion with co-factor 150
+  fcs_from_file[[i]]@exprs = asinh(fcs_from_file[[i]]@exprs/150) 
+  
+  # Median centering and scaling
+  center_scale = function(x) {
+    scale(x, center = apply(x,2,median), scale = T)
+  }
+  fcs_from_file[[i]]@exprs = center_scale(fcs_from_file[[i]]@exprs)
+  
+  # Also selecting the only the necessary markers in the parameters list as well
+  
+  # Re-order the list as per "re-order" list
+  fcs_from_file[[i]]@parameters@data[["name"]] =  re_order
+  
+  # Change the laser labels to antibody labels 
+  fcs_from_file[[i]]@parameters@data[["name"]] = c("FSC-A",
+                                                   "FSC-H",
+                                                   "FSC-W",
+                                                   "SSC-A",
+                                                   "SSC-H",
+                                                   "SSC-W",
+                                                   "FJComp-APC-A",
+                                                   "FJComp-APC-Cy7-A",
+                                                   "FJComp-Alexa Fluor 700-A",
+                                                   "FJComp-Brilliant Violet 421-A",
+                                                   "FJComp-Brilliant Violet 510-A",
+                                                   "FJComp-Brilliant Violet 605-A",
+                                                   "FJComp-FITC-A",
+                                                   "FJComp-PE-A",
+                                                   "FJComp-PE-Cy7-A",
+                                                   "FJComp-PerCP-Cy5-5-A",
+                                                   "FJComp-Qdot 585-A",
+                                                   "FJComp-Qdot 625-A",
+                                                   "FJComp-Qdot 705-A",
+                                                   "FJComp-Qdot 800-A" ,
+                                                   "Time" )
+
+  # Also selecting the only the necessary markers in the parameters list as well
+  re_select = c("FSC-A",
+                "SSC-A",
+                "FJComp-APC-A",
+                "FJComp-Alexa Fluor 700-A",
+                "FJComp-Brilliant Violet 421-A",
+                "FJComp-Brilliant Violet 510-A",
+                "FJComp-Brilliant Violet 605-A",
+                "FJComp-FITC-A",
+                "FJComp-PE-A",
+                "FJComp-PE-Cy7-A",
+                "FJComp-PerCP-Cy5-5-A",
+                "FJComp-Qdot 585-A",
+                "FJComp-Qdot 625-A",
+                "FJComp-Qdot 705-A")
+  fcs_from_file[[i]]@parameters@data = fcs_from_file[[i]]@parameters@data[fcs_from_file[[i]]@parameters@data$name %in% re_select,]
+  
+  # Save the new pre-processed FCS files 
+  write.FCS(fcs_from_file[[i]],
+            filename = paste0("~/Suzanne/Cytotree/NSCLC_late_stage_samples/Data/late_stage_nsclc/Corrected_samples/CLEAN_late_Mix 4_Tumor_NSCLC_with_comp_median_",donor_list[i],'.fcs'))
+}
+
